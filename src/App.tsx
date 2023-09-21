5d157986a9e0dc6b0e1e6fd7156012baf145aa14
adhxabre <abeldustin06@gmail.com>
2023-09-21 11:57:05 +0700

    <✨> Props (Destructured Props | Default Props)

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 
-// Variables
-// let doksli: string = "Kaela Esport";
-// console.log(doksli);
-
-// function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
-//   event.preventDefault();
-
-//   doksli = "Kaela dulunya player Esport";
-//   console.log(doksli);
-// }
-
-// ngebangun tipe data dari object state nya
-type State = {
-  doksli: string;
-};
-
-export default class App extends Component<State> {
-  state = {
-    doksli: "Kaela Esport",
-  };
-
-  updateDoksli = () => {
-    this.setState({
-      doksli: "Kaela dulunya player Esport",
-    });
-  };
-
+export default class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div>{this.state.doksli}</div>
-
-        <button onClick={this.updateDoksli}>Beri Informasi Baru!</button>
+        <div>Partai</div>
+        <Partai partai={"Partai Demokrat Indonesia Perjuangan"} />
       </React.Fragment>
+    );
+  }
+}
+
+class Partai extends React.Component<{ partai?: string }, { value: string }> {
+  render() {
+    // destructured props
+    const { partai } = this.props;
+
+    // default props
+    // const { partai = "PDIP" } = this.props;
+
+    return (
+      <>
+        <div>
+          <h1>{partai}</h1>
+        </div>
+      </>
     );
   }
 }
